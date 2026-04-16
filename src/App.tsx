f56371152cf67f2b06f3df613158c1d5d364138e
mrcandie <akinwumiadekanmi2@gmail.com>
2026-04-16 09:05:05 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const ProtectedRoute = () => {
 
   if (token && email && name) {
     login({ email, fullName: name }, token);
-    return <Navigate to="/" state={{ replace: true }} replace />;
+    return <Navigate to="/" replace />;
   }
 
   const unauth = ["/login", "/register"];
