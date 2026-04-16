cbd19dcfb141f82fa9c339fd44636d501b682d52
mrcandie <akinwumiadekanmi2@gmail.com>
2026-04-16 08:42:40 +0100

    google authentication

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,8 +1,27 @@
 import type React from "react";
 import Header from "./header";
 import SideBar from "./sidebar";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/auth";
+import { useEffect } from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const [searchParam] = useSearchParams();
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const token = searchParam.get("token");
+  const email = searchParam.get("email");
+  const name = searchParam.get("name");
+
+  useEffect(() => {
+    if (token && email && name) {
+      login({ email, fullName: name }, token);
+
+      navigate(window.location.pathname, { replace: true });
+    }
+  }, [name, email, token, login, navigate]);
+
   return (
     <div className="min-h-screen bg-white dark:bg-[#001c10] text-[#001c10] dark:text-white font-sans flex transition-colors duration-500">
       <SideBar />
